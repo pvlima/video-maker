b0454012dd255951fa85fe0e05f55f28b659c9c8
Paulo Vitor <pv.lima02@gmail.com>
2020-03-27 17:51:45 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const robots = {
   input: require('./robots/user-input'),
   text: require('./robots/text'),
   state: require('./robots/state'),
+  image: require('./robots/image'),
 }
 
 async function start() {
-
-  robots.input();
-  await robots.text();
+  // robots.input();
+  // await robots.text();
+  await robots.image();
 
   const content = robots.state.load();
   console.dir(content, { depth: null });
-  
 }
 
 start();
